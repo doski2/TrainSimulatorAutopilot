b8df6d16ab33ba823f279a4bd0bcfe5467d1e9e1
Train Simulator <doski@example.com>
2025-12-15 14:21:42 +0100

    Remove temporary debug scripts from repository and rely on .gitignore

--- tmp_health_test.py
+++
@@ -1,7 +0,0 @@
-from alert_system import AlertSystem
-asys = AlertSystem()
-asys.config['performance_degradation']['response_time_threshold_ms'] = None
-alerts = asys.perform_health_check()
-print('alerts count:', len(alerts))
-for a in alerts:
-    print(a.alert_id, a.title, a.data.get('error'))
